c66ca94605abcdcc87a65ac5e2d0f2628de6b62c
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-09-11 12:36:14 +0200

    StoringState: refactored to similar style as HarvesterState

--- src/creep/states/StoringEnergy.ts
+++ src/creep/states/StoringEnergy.ts
@@ -1,50 +1,76 @@
 import {MovingState, resolve, StateResolver} from "./CreepState";
 
-export function storeEnergy(creep: Creep, state: StateResolver) {
-  if (!creep.memory.storage) {
-    const spawn = creep.room.find(FIND_MY_STRUCTURES, {
-      filter: s => s.structureType === STRUCTURE_SPAWN && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-    });
-    if (spawn && spawn.length) {
-      creep.memory.storage = spawn[0].id;
-      creep.memory.targetPos = {
-        x: spawn[0].pos.x,
-        y: spawn[0].pos.y,
-        room: spawn[0].pos.roomName,
-      };
-    } else {
-      const storage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-        filter: s => (
-          (s.structureType === STRUCTURE_EXTENSION || s.structureType === STRUCTURE_LINK) &&
-          s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-        ) || (
-          s.structureType === STRUCTURE_STORAGE &&
-          s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-        )
-      });
-      if (storage) {
-        creep.memory.storage = storage.id;
-        creep.memory.targetPos = {
-          x: storage.pos.x,
-          y: storage.pos.y,
-          room: storage.pos.roomName,
-        };
-      } else {
-        // TODO idle
-      }
-    }
-    creep.memory.state = resolve({nextState: MovingState});
-    return;
-  }
+export function storeEnergy(creep: Creep, state: StateResolver): void {
   if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
     delete creep.memory.storage;
     creep.memory.state = resolve(state);
     return;
   }
-  const store = Game.getObjectById(creep.memory.storage as Id<StructureSpawn | StructureExtension | StructureLink | StructureStorage | StructureContainer>);
-  if (store) {
-    creep.transfer(store, RESOURCE_ENERGY);
-  } else {
-    delete creep.memory.storage;
+
+  if (!creep.memory.storage) {
+    assignStorage(creep);
+    creep.memory.state = resolve({nextState: MovingState});
+    return;
   }
+
+  const assignedStorage = Game.getObjectById(creep.memory.storage as Id<StructureSpawn | StructureExtension | StructureLink | StructureStorage | StructureContainer>);
+  if (!assignedStorage) {
+    assignStorage(creep);
+    return;
+  }
+
+  const transferResult = creep.transfer(assignedStorage, RESOURCE_ENERGY);
+  if (transferResult === OK) {
+    delete creep.memory.storage;
+    creep.memory.state = resolve(state);
+    return;
+  }
+  if (transferResult === ERR_NOT_IN_RANGE) {
+    creep.memory.state = resolve({nextState: MovingState});
+  }
+}
+
+function assignStorage(creep: Creep) {
+  const spawn = findSpawn(creep);
+  if (spawn) {
+    setTargetStorage(creep, spawn);
+  } else {
+    const storage = findClosestStorage(creep);
+    if (storage) {
+      setTargetStorage(creep, storage);
+    } else {
+      // TODO idle
+    }
+  }
+}
+
+function findSpawn(creep: Creep): Structure | undefined {
+  const spawns = creep.room.find(FIND_MY_STRUCTURES, {
+    filter: s => s.structureType === STRUCTURE_SPAWN && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+  });
+  if (spawns && spawns.length) return spawns[0];
+  return undefined;
+}
+
+function findClosestStorage(creep: Creep): Structure | undefined {
+  const storage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+    filter: s => (
+      (s.structureType === STRUCTURE_EXTENSION || s.structureType === STRUCTURE_LINK) &&
+      s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+    ) || (
+      s.structureType === STRUCTURE_STORAGE &&
+      s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+    )
+  });
+  if (storage) return storage;
+  return undefined;
+}
+
+function setTargetStorage(creep: Creep, storage: Structure): void {
+  creep.memory.storage = storage.id;
+  creep.memory.targetPos = {
+    x: storage.pos.x,
+    y: storage.pos.y,
+    room: storage.pos.roomName,
+  };
 }
