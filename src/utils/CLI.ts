ab510c368108b33b351139a5d7081bf1d22b09e3
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-09-16 11:30:11 +0200

    removed suicide from cli

--- src/utils/CLI.ts
+++ src/utils/CLI.ts
@@ -1,9 +1,16 @@
 export function cli() {
+  function getBodyCompositions() {
+    return Object.values(Game.creeps).map(c => c.body.map(b => b.type)).map(b => JSON.stringify(b) + "\n");
+  }
+
+  function getTicksToLive() {
+    return Object.values(Game.creeps).map(c => `${c.name}: ${c.ticksToLive}\n`);
+  }
+
   return {
     creeps: {
-      bodies: Object.values(Game.creeps).map(c => c.body.map(b => b.type)).map(b => JSON.stringify(b) + "\n"),
-      suicide: Object.values(Game.creeps).forEach(c => c.suicide()),
-      life: Object.values(Game.creeps).map(c => `${c.name}: ${c.ticksToLive}\n`)
+      bodies: getBodyCompositions(),
+      life: getTicksToLive()
     }
   }
 }
