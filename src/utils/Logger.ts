2831e7ccd2278fdbb00265250261853228a6d712
tbielaszewski <tbielaszewski@objectivity.co.uk>
2020-11-14 17:22:08 +0100

    custom logger

---
+++ src/utils/Logger.ts
@@ -0,0 +1,26 @@
+export interface Logger {
+  log: (...messages: string[]) => void
+}
+
+class NamedLogger implements Logger {
+  private readonly name: string
+
+  public constructor(name: string) {
+    this.name = name;
+  }
+
+  public log(...messages: string[]): void {
+    if (_.get(Memory.log, this.name) === true) {
+      const message = messages.reduce((a, b) => a + " " + b, "")
+      console.log(`[${Game.time}] ${message}`)
+    }
+  }
+}
+
+export function getLogger(name: string): Logger {
+  return new NamedLogger(name)
+}
+
+export function log(name: string): (...messages: string[]) => void {
+  return getLogger(name).log
+}
