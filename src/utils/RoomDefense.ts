65efac2207f6a445c0761215edd2673c57fed18a
tomekbielaszewski <tomekbielaszewski@gmail.com>
2023-10-11 20:36:05 +0200

    tower repairs

--- src/utils/RoomDefense.ts
+++ src/utils/RoomDefense.ts
@@ -1,7 +1,13 @@
 export function defendRoom(room: Room) {
-  const hostiles = room.find(FIND_HOSTILE_CREEPS);
-  if (hostiles.length > 0) {
-    const towers = room.find<StructureTower>(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
-    towers.forEach(tower => tower.attack(hostiles[0]));
-  }
+  const towers = room.find<StructureTower>(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
+  towers.forEach(tower => {
+    const hostiles = room.find(FIND_HOSTILE_CREEPS);
+    if (hostiles.length > 0) {
+      tower.attack(hostiles[0]);
+    } else {
+      const structure = room.find(FIND_MY_STRUCTURES)
+        .find(s => s.hits / s.hitsMax < 0.79)
+      if (structure) tower.repair(structure)
+    }
+  });
 }
